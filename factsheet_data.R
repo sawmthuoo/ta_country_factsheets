1e02d6114df7c096e5f4077fe913d4004f5212ff
sawmthuoo <saw.mthuoo@gmail.com>
2025-11-06 16:19:21 -0600

    number of smokers and youth prev  added

--- factsheet_data.R
+++ factsheet_data.R
@@ -66,7 +66,7 @@ cost_lcu <- df_corre_red1 |>
   ) |> 
   mutate(
     iso3 = countrycode(ctry, "country.name.en", "iso3c"),
-    cost = format(as.numeric(cost) * 1e06, scientific = FALSE, big.mark = ",")
+    cost = format(as.numeric(cost) * 1e06, scientific = FALSE, trim = TRUE, big.mark = ",")
   ) |> 
   select(iso3, cost)
 
@@ -97,7 +97,7 @@ prev <- df_fs |>
     link_death
   )
 
-# smoking prevalence
+#---- 2.1 smoking prevalence ----
 ## Data Source: WHO GHO 2024
 df_smk_prev <- read_csv("data/smk_prev_who_20250902.csv") |> 
   select(
@@ -112,7 +112,7 @@ df_smk_prev <- read_csv("data/smk_prev_who_20250902.csv") |>
     year == 2025
   )
 
-smk_prev <- df |> select(ISO3, ctry = Countrynameforthepage) |> 
+fn_smk_prev <- df |> select(ISO3, ctry = Countrynameforthepage) |> 
   left_join(df_smk_prev, join_by(ISO3)) |> 
   mutate(val = paste0(val,"%")) |> 
   pivot_wider(
@@ -130,9 +130,95 @@ smk_prev <- df |> select(ISO3, ctry = Countrynameforthepage) |>
   ) |> 
   select(ISO3, men, women, prev_text)
 
-# smoker number
+#---- 2.2 smoker number ----
 ## Data Source: WHO trend report, 2025
 ## Data table is converted into Excel
+read_smk_num_data <- function(cell_range){
+  read_excel(
+    "data/num_smokers_who_2024.xlsx",
+    sheet = "Table 1",
+    range = {{ cell_range }},
+    col_names = FALSE
+  ) |> 
+    clean_names() |> 
+    remove_empty()
+}
+
+clean_smk_num_data <- function(df, iso, tubth, tuml, tufml, tsbth, tsml, tsfml){
+  df |> 
+    select(
+      ISO3 = {{ iso }},
+      tbc_user_bth = {{ tubth}},
+      tbc_user_male = {{ tuml }},
+      tbc_user_female = {{ tufml }},
+      tbc_smk_bth = {{ tsbth }},
+      tbc_smk_male = {{ tsml }},
+      tbc_smk_female = {{ tsfml }}
+    ) |> 
+    filter(!is.na(ISO3)) |> 
+    mutate(
+      across(starts_with("tbc"),
+             \(x) parse_number(
+               as.character(x), locale = locale(decimal_mark = ".", grouping_mark = " ")))
+    )
+}
+
+df_smk1 <- read_smk_num_data("A5:AK40")
+df_smk1_cln <- clean_smk_num_data(df_smk1, x6, x13, x18, x24, x28, x32, x35)
+
+df_smk2 <- read_smk_num_data("A44:AK79")
+df_smk2_cln <- clean_smk_num_data(df_smk2, x3, x10, x16, x21, x25, x29, x33)
+
+df_smk3 <- read_smk_num_data("A83:AK118")
+df_smk3_cln <- clean_smk_num_data(df_smk3, x8, x15, x20, x24, x28, x32, x35)
+
+df_smk4 <- read_smk_num_data("A122:AK157")
+df_smk4_cln <- clean_smk_num_data(df_smk4, x5, x12, x17, x22, x26, x30, x33)
+
+df_smk5 <- read_smk_num_data("A161:AK196")
+df_smk5_cln <- clean_smk_num_data(df_smk5, x4, x12, x17, x22, x26, x30, x34)
+
+df_smk6 <- read_smk_num_data("A200:AK218")
+df_smk6_cln <- clean_smk_num_data(df_smk6, x7, x14, x19, x23, x27, x31, x35)
+
+df_smk_num <- bind_rows(df_smk1_cln,
+                        df_smk2_cln,
+                        df_smk3_cln, 
+                        df_smk4_cln, 
+                        df_smk5_cln, 
+                        df_smk6_cln)
+
+fn_smk_num <- df |> select(ISO3, ctry = Countrynameforthepage) |> 
+  left_join(df_smk_num, join_by(ISO3)) |>
+  select(
+    ctry,
+    ISO3,
+    num_men = tbc_smk_male,
+    num_women = tbc_smk_female,
+    num_both = tbc_smk_bth
+  ) |> 
+  mutate(
+    across(starts_with("num"),\(x) format(x * 1e03, trim = TRUE, big.mark = ",")),
+    num_text = paste0("Number of adult smokers in ",ctry," is ",num_both,".")
+  ) |> 
+  select(
+    ISO3, num_men, num_women, num_text
+  )
+  
+# 2.3 youth ----
+df_yth_smk_prv <- read_csv("data/youth_smk_ihme_20250404.csv") |> 
+  filter(year_id == 2023) |> 
+  select(ctry = location_name, sex = sex_id, val) |> 
+  mutate(
+    ISO3 = countrycode(ctry, "country.name.en", "iso3c"),
+    .after = ctry
+  ) |> 
+  mutate(
+    sex = case_when(
+      sex == 1 ~ "boy",
+      sex == 2 ~ "girl"
+    )
+  )
 
 
 # 3. Negative effect of tbc use ----
